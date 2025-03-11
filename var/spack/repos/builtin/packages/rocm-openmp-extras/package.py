e312b6b286c13d4ac56c9a1d4cdce71b6460f95e
Afzal Patel <Afzal.Patel@amd.com>
2025-03-11 10:56:58 -0700

    rocm-openmp-extras: add develop patch

--- var/spack/repos/builtin/packages/rocm-openmp-extras/package.py
+++ var/spack/repos/builtin/packages/rocm-openmp-extras/package.py
@@ -391,7 +391,8 @@ class RocmOpenmpExtras(Package):
         when="@6.1",
     )
     patch("0001-Avoid-duplicate-registration-on-cuda-env.patch", when="@6.1")
-    patch("0001-Avoid-duplicate-registration-on-cuda-env-6.2.patch", when="@6.2:")
+    patch("0001-Avoid-duplicate-registration-on-cuda-env-6.2.patch", when="@6.2:6.3")
+    patch("0001-Avoid-duplicate-registration-on-cuda-env-develop.patch", when="@develop")
 
     def setup_run_environment(self, env):
         devlibs_prefix = self.spec["llvm-amdgpu"].prefix
